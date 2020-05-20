d30bd7b07802f8ebe60315f5e04b3caa7ce815bf
unknown <1717ni@gmail.com>
2020-05-20 08:29:44 -0400

    Committing additional changes to binary to decimal problem solution

--- JavaConcepts/src/javaInterviewQuestions/BinaryToDecimal38.java
+++ JavaConcepts/src/javaInterviewQuestions/BinaryToDecimal38.java
@@ -10,11 +10,14 @@ public class BinaryToDecimal38 {
 		Scanner scan = new Scanner(System.in);
 		String str = scan.nextLine();
 		scan.close();
+		//1. binary to decimal 1
 		int decNo = Integer.parseInt(str, 2);
 		System.out.println("Converted Decimal number :: " + decNo);
 
 		System.out.println("---------------------------------------");
 		printDecimalToBinary(decNo);
+		System.out.println("---------------------------------------");
+		printBinaryToDecimal(Integer.parseInt(str));
 	}
 
 	public static void printDecimalToBinary(int decNo) {
@@ -25,8 +28,23 @@ public class BinaryToDecimal38 {
 			decNo /= 2;
 			i++;
 		}
-		for (int j = i-1; j >=0; j--) {
+		for (int j = i - 1; j >= 0; j--) {
 			System.out.print(binaryNum[j]);
 		}
+	}
+
+	public static void printBinaryToDecimal(int binNum) {
+
+		int decNo = 0;
+		int temp = binNum;
+		int base = 1; // 2^1 = 1;
+
+		while (temp > 0) {
+			int lastDigit = temp % 10;
+			temp /= 10;
+			decNo += lastDigit * base;
+			base *= 2;
+		}
+		System.out.println("Binary to Decimal number :: " + decNo);
 	}
 }
